598bde89a8f99b6181751f62501ec06528dc45c9
Aaron Delate <aaron@zero-one.io>
2025-05-22 18:17:07 +0200

    crimesjohnson: lastName is optional

--- validators/trainers.mjs
+++ validators/trainers.mjs
@@ -3,7 +3,7 @@ import { body } from "express-validator"
 export const trainerValidationRules = () => {
   return [
     body('firstName').notEmpty().withMessage("firstName is required!"),
-    body('lastName').notEmpty().withMessage("lastName is required!"),
+    body('lastName').optional(),
     body('age').toInt().notEmpty().withMessage("age is required!"),
     body('gender').notEmpty().withMessage("gender is required!"),
     body('region').notEmpty().withMessage("region is required!"),
