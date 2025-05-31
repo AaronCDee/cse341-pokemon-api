531509e28ede531eec598beffe531824656e66d5
Aaron Delate <aaron@zero-one.io>
2025-05-31 10:00:19 +0200

    crimesjohnson:refactor: dynamically produce host

--- routes/swagger.js
+++ routes/swagger.js
@@ -6,6 +6,14 @@ const swaggerDocument = require('../swagger.json');
 const router = express.Router();
 
 router.use('/', swaggerUi.serve);
-router.get('/', swaggerUi.setup(swaggerDocument));
+router.get('/', (req, res, next) => {
+  const swaggerWithHost = {
+    ...swaggerDocument,
+    host: req.get('host'),
+    schemes: [req.protocol],
+  };
+
+  return swaggerUi.setup(swaggerWithHost)(req, res, next);
+});
 
 module.exports = router;
