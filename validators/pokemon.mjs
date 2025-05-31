efa72de424040e7738fd1d350ef7015b065ea795
Aaron Delate <aaron@zero-one.io>
2025-05-31 10:00:56 +0200

    crimesjohnson:update validations

--- validators/pokemon.mjs
+++ validators/pokemon.mjs
@@ -5,6 +5,9 @@ export const pokemonValidationRules = () => {
     body('name').notEmpty().withMessage("name is required!"),
     body('type').notEmpty().withMessage("type is required!"),
     body('weakness').notEmpty().withMessage("weakness is required!"),
-    body('caughtAt').toDate().notEmpty().withMessage("caughtAt is required!"),
+    body('caughtAt').toDate().notEmpty().withMessage("caughtAt must be a date and is required!"),
+    body('level').toInt().notEmpty().withMessage("level must be an integer and is required!"),
+    body('trainer').notEmpty().withMessage("trainer is required!"),
+    body('abilities').toArray().notEmpty().withMessage("abilities must be an array and is required!")
   ]
 }
