7d2c8b1affca0bc882f4d991ef3a19431f6bc52f
Aaron Delate <aaron@zero-one.io>
2025-05-31 10:00:02 +0200

    crimesjohnson:refactor: include auth for relevant routes

--- routes/pokemon.mjs
+++ routes/pokemon.mjs
@@ -3,13 +3,14 @@ import express from 'express';
 import { index, show, create, update, destroy } from '../controllers/pokemon.mjs';
 import { pokemonValidationRules }               from '../validators/pokemon.mjs';
 import validate                                 from '../middleware/validate.mjs';
+import { isAuthenticated }                      from '../middleware/authenticate.mjs';
 
 const router = express.Router();
 
 router.get('/', index);
 router.get('/:id', show);
-router.post('/', pokemonValidationRules(), validate, create);
-router.put('/:id', pokemonValidationRules(), validate, update);
-router.delete('/:id', destroy);
+router.post('/', isAuthenticated, pokemonValidationRules(), validate, create);
+router.put('/:id', isAuthenticated, pokemonValidationRules(), validate, update);
+router.delete('/:id', isAuthenticated, destroy);
 
 export default router;
--- routes/trainers.mjs
+++ routes/trainers.mjs
@@ -3,13 +3,14 @@ import express from 'express';
 import { index, show, create, update, destroy } from '../controllers/trainers.mjs';
 import { trainerValidationRules }               from '../validators/trainers.mjs';
 import validate                                 from '../middleware/validate.mjs';
+import { isAuthenticated }                      from '../middleware/authenticate.mjs';
 
 const router = express.Router();
 
 router.get('/', index);
 router.get('/:id', show);
-router.post('/', trainerValidationRules(), validate, create);
-router.put('/:id',trainerValidationRules(), validate, update);
-router.delete('/:id', destroy);
+router.post('/', isAuthenticated, trainerValidationRules(), validate, create);
+router.put('/:id', isAuthenticated, trainerValidationRules(), validate, update);
+router.delete('/:id', isAuthenticated, destroy);
 
 export default router;
